d73f8ea8219b449bac01e27bb8a4fd946b8f2736
Chinese0 <891278127@qq.com>
2021-02-16 23:29:27 +0800

    嘿嘿

---
+++ 2月16日.js
@@ -0,0 +1,11 @@
+// 什么是冒泡排序算法
+function maoPao(arr) {
+  let max
+
+  for (let i = 0; i < arr.length; i++) {
+    max = arr[i]
+    for (let j = 0, l = arr.length - i - 1; j++;) {
+      if ( < )
+    }
+  }
+}
