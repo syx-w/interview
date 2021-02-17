7f73be557ec8d227d8e70640d3dfdebc79681c18
Chinese0 <891278127@qq.com>
2021-02-18 07:08:36 +0800

    初六

--- 31-排序.js
+++ 31-排序.js
@@ -1,3 +1,4 @@
+// 冒泡排序、插入排序、选择排序、快速排序、归并排序、计数排序、基数排序、桶排序
 function swap(arr, i, j) {
   let temp = arr[i]
   arr[i] = arr[j]
@@ -16,5 +17,30 @@ function bubbleSort(arr) {
   return arr
 }
 
-// 选择排序
+// 选择排序  (挑一个最小的放到最前面)
+function quickSort(arr) {
+  if (arr == null || arr.length == 0) return []
+  for (let i = 1; i < arr.length; i++) {
+    let min = i
+    for (let j = i + 1; j <= arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j
+      }
+    }
+    if (i !== min) {
+      swap(arr, i, min)
+    }
+  }
+  return arr
+}
+// 插入排序 （打牌-最大的放后面）
+function insertSort(arr) {
+  if (arr == null || arr.length == 0) return []
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i - 1; j < arr.length && arr[j] > arr[j + 1]; j++) {
+      swap(arr, j, j + 1)
+    }
+  }
+  return arr
+}
 // 
--- xiaoceshi.js
+++ xiaoceshi.js
@@ -1,17 +1,22 @@
-// promise.all()的应用
+function insertsort(arr) {
+  if (arr == null || arr.length == 0) return []
+  for (let i = 1; i < arr.length; i++) {
+    let min = i
+    for (let j = i + 1; j <= arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j
+      }
+    }
+    if (i !== min) {
+      swap(arr, i, min)
+    }
+  }
+  return arr
+}
 
-let a = new Promise(() => {
-  setTimeout(() => {
-    return '我是a'
-  }, 1000)
-})
-let b = new Promise(() => {
-  setTimeout(() => {
-    return '我是b'
-  }, 2000)
-})
-Promise.all([a, b]).then((resolve) => {
-  console.log(resolve);
-}, (reject) => {
-  console.log('我错了');
-})
+function swap(arr, i, j) {
+  let temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+console.log(insertsort([1, 5, 2, 7, 3, 8, 4]));
