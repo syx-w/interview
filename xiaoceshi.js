05a3a5ec8bbb8d181dd6385be02e37c30009bdec
Chinese0 <891278127@qq.com>
2021-02-19 08:13:08 +0800

    新的一天

--- xiaoceshi.js
+++ xiaoceshi.js
@@ -1,24 +1,6 @@
-function quickSort(arr) {
-  if (arr.length < 2) return arr
-  let leftArr = [],
-    rightArr = [],
-    curArr = [arr[0]],
-    cur = arr[0]
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > cur) {
-      rightArr.push(arr[i])
-    } else if (arr[i] === cur) {
-      curArr.push(arr[i])
-    } else {
-      leftArr.push(arr[i])
-    }
-  }
-  return [...quickSort(leftArr), ...curArr, ...quickSort(rightArr)]
-}
-
-function swap(arr, i, j) {
-  let temp = arr[i]
-  arr[i] = arr[j]
-  arr[j] = temp
-}
-console.log(quickSort([1, 5, 2, 7, 3, 8, 4]));
+let a = new Array()
+// 手写一个new到底发生了什么
+let a = {}
+a.__proto__ = Array.prototype
+Array.call(a)
+return a
