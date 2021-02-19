ed5e6ee40676edf72d5578711e5b89254a82418a
Chinese0 <891278127@qq.com>
2021-02-19 10:15:46 +0800

    新的一天

--- xiaoceshi.js
+++ xiaoceshi.js
@@ -1,6 +1,14 @@
-let a = new Array()
+/* let a = new Array()
 // 手写一个new到底发生了什么
 let a = {}
 a.__proto__ = Array.prototype
 Array.call(a)
-return a
+return a */
+var b = 4
+d = 'aaa' // <=> window.d
+
+// ES6新增
+let a = [1, 2, 3]
+a = 4;
+const c = 'ddd'
+c = 3
