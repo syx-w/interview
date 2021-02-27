cc29381f2cc82db3cfe4c617c5adb4f1d314815d
Chinese0 <891278127@qq.com>
2021-02-27 18:27:05 +0800

    url

--- x凡科春招.js
+++ x凡科春招.js
@@ -1,10 +1,10 @@
-/* 
-第三题：call方法实现：
-这里我使用Symbol方法来作为唯一属性，引发了面试官后面给的题
-*/
-
-
-// 然后就引发出来为啥使用Symbol， 还可以用其他实现唯一key吗？ 说一下
-function myCall(a) {
-  this.my
+// 第七题： 在函数内部如何判断当前函数是被执行还是被new了？
+function a() {
+  if (this instanceof a) {
+    console.log('被new');
+  } else {
+    console.log('被执行');
+  }
 }
+let b = new a()
+a()
