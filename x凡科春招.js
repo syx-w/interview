21ee198d5e4c768521ecb1e4454a629cba48ee43
Chinese0 <891278127@qq.com>
2021-02-27 17:11:56 +0800

    凡科

--- x凡科春招.js
+++ x凡科春招.js
@@ -1,27 +1,10 @@
-function fn() {
-  getName = function () {
-    console.log('yifang')
-  }
-  return this;
-}
-fn.getName = function () {
-  console.log('liudehua')
-}
-fn.prototype.getName = function () {
-  console.log('zhangxueyou')
-}
-var getName = function () {
-  console.log('zhouxingci')
-}
-
-function getName() {
-  console.log('huangzesi')
-}
-// 依次以下代码分别输出什么 fn().getName(); fn.getName(); getName(); new fn.getName(); new new fn.getName();
 /* 
-fn().getName()   huangzesi 
-fn.getName() liudehua
-getName()  zhouxingci
-new fn.getName()  zhangxueyou
-new new fn.getName() zhangxueyou
+第三题：call方法实现：
+这里我使用Symbol方法来作为唯一属性，引发了面试官后面给的题
 */
+
+
+// 然后就引发出来为啥使用Symbol， 还可以用其他实现唯一key吗？ 说一下
+function myCall(a) {
+  this.my
+}
