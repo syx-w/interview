335b0d5a9c8ba89513f0510d7e313ede31706db8
Chinese0 <891278127@qq.com>
2021-03-05 23:15:12 +0800

    1

--- React学习/src/index.js
+++ React学习/src/index.js
@@ -1,1 +1,10 @@
-console.log('haha');
+function bytesLength(str) {
+  var count = str.length;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 255) {
+      count++;
+      console.log(str.length)
+    }
+  }
+}
+var by = bytesLength("wode 名字")
