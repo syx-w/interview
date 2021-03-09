a493b2337bd5d5f822d653f82794a25d819860cb
Chinese0 <891278127@qq.com>
2021-03-09 22:33:59 +0800

    mian shi

--- learn/React学习/webpack.config.js
+++ learn/React学习/webpack.config.js
@@ -1,23 +1,22 @@
 // webpack-dev-server 将打包好的main.js托管到内存中
 
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin') // 导入 在页面中生成index页面的插件
+let path = require('path')
+let HtmlWebPackPlugin = require('html-webpack-plugin') // 导入 在页面中生成index页面的插件
 // 创建一个插件的实例化对象
-const htmlPlugin = new HtmlWebPackPlugin({
+let htmlPlugin = new HtmlWebPackPlugin({
   template: path.join(__dirname, './scr/index.html'), // 源文件
   filename: 'index.html' // 生成的内存中的首页的名字
 })
 module.exports = {
-  entry: './index.js',
   mode: 'development',
   plugins: [
     htmlPlugin,
-
   ],
-  rules: [{
-    test: '/\.html$/',
-    use: [{
-      loader: 'html-loader'
+  entry: './src/index', //入口
+  module: {
+    rules: [{
+      test: '/\.html$/i',
+      use: ['html-loader']
     }]
-  }]
+  }
 }
